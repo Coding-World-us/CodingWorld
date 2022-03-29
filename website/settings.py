d034b845b37e3d20cd66c777e3153c674596f735
Coding-World-us <102560660+Coding-World-us@users.noreply.github.com>
2022-03-29 23:37:59 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'codingworld412@gmail.com'
+EMAIL_HOST_PASSWORD = 'python412'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
